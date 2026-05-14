977693e4086e8f26f4445a6d171f69233f314e64
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-14 17:05:49 +0530

    redo greatest target element

---
+++ Redo/src/com/sunil/redo/Letter.java
@@ -0,0 +1,26 @@
+package com.sunil.redo;
+
+public class Letter {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        char ans = search(letters,target);
+        System.out.println(ans);
+    }
+
+    static char search(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            //finding the mid val.
+            int mid = start + (end - start) / 2;
+            if (letters[mid] > target) {
+                //if mid is greater than target we go right to left to possible smallest greatest number that is end = mid -1;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
