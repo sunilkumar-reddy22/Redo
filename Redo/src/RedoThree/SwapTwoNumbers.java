44589000b54d8ded8bcaea2471314cce3aeeaa72
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-09-03 06:11:21 +0530

    basic code

---
+++ Redo/src/RedoThree/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package RedoThree;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int temp = 0;
+        int a = 10;
+        int b = 5;
+        System.out.println(a + " " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
